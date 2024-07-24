e22a750bd3a5ecb3670cbdeb20dd4f5f9cf9e7d3
Egor Petko <egrpetko@gmail.com>
2024-07-24 17:59:35 +0300

    Added new models

---
+++ OnlineLibrary.Core/Models/Interfaces/IAuthorInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineLibrary.Core.Models.Interfaces
+{
+    internal interface IAuthorInfo
+    {
+        int Id { get; }
+        string Name { get; }
+    }
+}
